94a92baebacada953d038eb6c69fa47d7b3c00e5
jhoaking <wawxper08@gmail.com>
2025-05-01 13:04:29 -1100

    añadinedo rutas protegidas front

--- Backend/src/routes/auth.routes.ts
+++ Backend/src/routes/auth.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { AuthController } from "../controller/AuthController";
 import { verify } from "../middlewares/verifyTokenMiddleware";
+import { permitRoles } from "../middlewares/permisionRolesMiddleware";
 
 export const AuthRoutes = Router();
 
 AuthRoutes.post("/register", AuthController.register);
 AuthRoutes.post("/login", AuthController.login);
 AuthRoutes.post("/", AuthController.logout);
-AuthRoutes.get("/me", verify, AuthController.getCurrentUser);
+AuthRoutes.get(
+  "/me",
+  verify,
+  permitRoles("admin"),
+  AuthController.getCurrentUser
+);
